5d22d64458334ae1265f25a6baf6e25aebbcfb16
Legarde2 <gus15290105@gmail.com>
2025-11-10 14:10:46 +0000

    Adicionando conteúdo

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
 
+import platform
+import os
+import psutil
 import json
 
+# Uso de CPU por núcleo
+print(psutil.cpu_percent(percpu=True))
+
+# Memória
+print(psutil.virtual_memory().used // 1024 ** 2, "MB")
+
+# PID do processo
+print(os.getpid())
+
+# SO
+print(platform.platform())
 
 APP = Flask(__name__)
 
@@ -9,8 +23,25 @@ APP = Flask(__name__)
 def info():
     return json.dumps([
         {'integrantes': [
-                'Gustavo Lisboa Prestes',
+                'Gustavo Lisboa',
                 'Vinicius'
             ]
+        }
+    ])
+
+@APP.get("/metrica")
+def metrica():
+    cpu_usage_per_core = psutil.cpu_percent(percpu=True)
+    memory_used_mb = psutil.virtual_memory().used // 1024 ** 2
+    process_id = os.getpid()
+    operating_system = platform.platform()
+
+    return json.dumps([
+        {'metricas': {
+            "cpu_usage_per_core": cpu_usage_per_core,
+            "memory_used": f"{memory_used_mb} MB",
+            "process_id": process_id,
+            "operating_system": operating_system
+            }
         }
     ])
