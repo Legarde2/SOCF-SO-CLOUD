81405c9f2b8bebc1c6068cce95c35bda2fdf14ad
Legarde2 <gus15290105@gmail.com>
2025-11-10 13:34:37 +0000

    Adicionando conteúdo

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+import json
+
+
+APP = Flask(__name__)
+
+@APP.get("/info")
+def info():
+    return json.dumps([
+        {'integrantes': [
+                'Gustavo Lisboa Prestes',
+                'Vinicius'
+            ]
+        }
+    ])
